600a1f758251ecea984178bb6c6d94aa1c093fba
mourodrigo <rodrigobt20@gmail.com>
2015-10-03 15:48:21 -0300

    Added examples

---
+++ Programacao-socket-e-roteamento/Defaults.h
@@ -0,0 +1,13 @@
+//
+//  Defaults.h
+//  Programacao-socket-e-roteamento
+//
+//  Created by Rodrigo Bueno Tomiosso on 01/10/15.
+//  Copyright (c) 2015 mourodrigo. All rights reserved.
+//
+
+#ifndef Programacao_socket_e_roteamento_Defaults_h
+#define Programacao_socket_e_roteamento_Defaults_h
+#include <string.h>
+
+#endif
--- Programacao-socket-e-roteamento/main.c
+++ Programacao-socket-e-roteamento/main.c
@@ -7,10 +7,9 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    
-    return 0;
-}
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    
+//    return 0;
+//}
